36c0215693b9ef63ea43b468e6d0ee9c3548f095
Caden Jurkovski <jurko036@umn.edu>
2024-12-09 13:17:11 -0600

    finished init

---
+++ Day_5/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self, input_file: str):
+        self.page_map = {}
+        self.page_orders = []
+        
+        with open(input_file, 'r') as file:
+            reached_newline = False
+
+            for line in file:
+                line = line.strip()
+                if reached_newline:
+                    self.page_orders.append(list(map(int, line.split(','))))
+                elif not line:
+                    reached_newline = True
+                    continue
+                else:
+                    x, y = line.split("|")
+                    self.page_map[int(x)] = int(y)
+
+if __name__ == "__main__":
+    solution = Solution("./data/input_small.txt")
+    print(solution.page_map)
+    print(solution.page_orders)
